3a261eb477eeae7bc63eee2d914d3321f5734edd
sachinsreedar92 <sachinsreedar92@gmail.com>
2019-02-13 18:43:50 +0530

    Binary Tree Terminology

--- src/main/java/com/interview/programs/trees/BinaryTree.java
+++ src/main/java/com/interview/programs/trees/BinaryTree.java
@@ -1,6 +1,15 @@
 package com.interview.programs.trees;
 
-// A Java program to introduce Binary Tree
+//    More tree terminology:
+//
+//        The depth of a node is the number of edges from the root to the node.
+//        The height of a node is the number of edges from the node to the deepest leaf.
+//        The height of a tree is a height of the root.
+//        A full binary tree.is a binary tree in which each node has exactly zero or two children.
+//        A complete binary tree is a binary tree, which is completely filled, with the possible exception of the bottom level, which is filled from left to right.
+
+
+//This is a Binary Tree ONLY
 
 public class BinaryTree
 {
