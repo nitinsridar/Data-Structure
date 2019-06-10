c050a9ea1fff84505d23007bfce5a1cf071c4f7d
sachinsreedar92 <sachinsreedar92@gmail.com>
2019-06-10 11:14:25 +0530

    Java8Features

---
+++ src/main/java/com/interview/programs/Java8Features/Streams.java
@@ -0,0 +1,17 @@
+package com.interview.programs.Java8Features;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Streams {
+
+    public static void main(String[] args) {
+
+        List<Integer> number = Arrays.asList(2,3,4,5);
+        List<Integer> square = number.stream().map(x->x*x).collect(Collectors.toList());
+        System.out.println(square);
+
+    }
+
+}
